1c1f4a974b082c652eae339cbbdb8c8a710ce3cc
MURAKAMI Masahiko <fossamagna2@gmail.com>
2023-03-13 09:22:52 +0900

    fix: use globSync instead of glob.sync

--- src/util.ts
+++ src/util.ts
@@ -1,9 +1,9 @@
 import * as path from 'path';
 import * as fs from 'fs';
-import glob from 'glob';
+import * as glob from 'glob';
 
 export function globSync(patterns: string[]): string[] {
-  return patterns.reduce((acc, pattern) => acc.concat(glob.sync(pattern)), [] as string[])
+  return patterns.reduce((acc, pattern) => acc.concat(glob.globSync([pattern])), [] as string[])
 }
 
 export function findConfig(
