77288276d30fc1101fe982e8457ebdb79395efcc
mato533 <35281743+mato533@users.noreply.github.com>
2023-07-12 09:06:20 +0900

    refactor: Redact the logic for searching source code files and NamedExportFiles (#210)
    * refactor: Refact the logic for file search
    
    * chore: Add the @types/jest
    
    * refactor: Remove unnecessary imports for jest
    
    ---------
    
    Co-authored-by: MURAKAMI Masahiko <fossamagna2@gmail.com>

--- src/util.ts
+++ src/util.ts
@@ -1,52 +1,34 @@
-import * as path from 'path';
-import * as fs from 'fs';
-import * as glob from 'glob';
+import path from 'path';
+import fs from 'fs';
+import glob from 'glob';
 
 export const getSrcFiles = (sources: string[]): string[] => {
-  const patterns = getAbsolutePathGlobPatterns(sources).map((arg) => {
-    const posixPath = process.platform === 'win32' ? getPosixPath(arg) : arg;
-    return [posixPath, '**/*.ts'].join(path.posix.sep);
+  const patterns = sources.map((arg) => {
+    const posixabsPath = getAbsolutePath(getPosixPath(arg));
+    return [posixabsPath, '**/*.ts'].join(path.posix.sep);
   });
-  return globSync(patterns);
+  return glob.globSync(patterns);
 };
 
-export const getNamedExportsPatterns = (
-  namedExportsFiles: string[]
-): string[] => {
-  const patterns = namedExportsFiles.map((arg) => {
-    return path.isAbsolute(arg) ? arg : convertAbsPath(arg);
-  });
-  return globSync(patterns);
+export const getNamedExportsFiles = (namedExportsGlobs: string[]): string[] => {
+  const patterns = namedExportsGlobs.map((arg) => getAbsolutePath(arg));
+  return glob.globSync(patterns);
 };
 
-export const getAbsolutePathGlobPatterns = (patterns: string[]): string[] => {
-  return patterns.map((arg) => {
-    return path.isAbsolute(arg) ? getPosixPath(arg) : convertAbsPath(arg);
-  });
+const getAbsolutePath = (posixPath: string): string => {
+  return path.isAbsolute(posixPath) ? posixPath : convertAbsPath(posixPath);
 };
 
-const convertAbsPath = (relativePath: string): string => {
+const convertAbsPath = (globPattern: string): string => {
   const normalizeCwd = getPosixPath(process.cwd());
-  return [normalizeCwd, relativePath].join(path.posix.sep);
+  return [normalizeCwd, globPattern].join(path.posix.sep);
 };
 
 const getPosixPath = (filePath: string): string => {
-  if (process.platform === 'win32') {
-    return filePath.split(path.win32.sep).join(path.posix.sep);
-  } else {
-    return filePath;
-  }
+  return process.platform === 'win32'
+    ? filePath.split(path.win32.sep).join(path.posix.sep)
+    : filePath;
 };
-
-function globSync(patterns: string[]): string[] {
-  return patterns.reduce((acc, pattern) => {
-    return acc.concat(
-      glob.globSync([pattern]).map((file) => {
-        return path.normalize(file);
-      })
-    );
-  }, [] as string[]);
-}
 
 export function findConfig(
   baseDir: string,
